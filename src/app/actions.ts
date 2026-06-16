ce6a45d6016ad8f1bb972f121880b2d76443ccff
OGLadi <yorubaherdsmehnes@gmail.com>
2026-06-16 18:02:08 +0100

    fix: remove strict supabase types for build

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,7 +3,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from '../types/supabase';
 
-const supabase = createClient<Database>(
+const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
